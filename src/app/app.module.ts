dd39b0c3b874437a7ce289ac37d0a3f40ee6c600
Sikandar Ali <sikandarali94@hotmail.com>
2019-03-07 14:59:27 +1100

    created components for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SearchBarComponent } from './main/search-bar/search-bar.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { ResultsComponent } from './main/results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SearchBarComponent,
+    FooterComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
