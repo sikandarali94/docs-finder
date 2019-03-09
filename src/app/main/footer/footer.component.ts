896b90c199561b2c91bf586d19737e73a3056bb6
Sikandar Ali <sikandarali94@hotmail.com>
2019-03-09 13:47:04 +1100

    get current year for footer copyright statement

--- src/app/main/footer/footer.component.ts
+++ src/app/main/footer/footer.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  date: Date = new Date(); // Store the current date.
+  year: number; // Stores the current full year.
 
   constructor() { }
 
   ngOnInit() {
+    this.year = this.date.getFullYear(); // Store the full year of the current date.
   }
 
 }
