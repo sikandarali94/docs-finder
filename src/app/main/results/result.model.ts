61fa61229ef6e62a40336cb4481dc846d51adfd5
Sikandar Ali <sikandarali94@hotmail.com>
2019-03-09 13:15:04 +1100

    modified data coming from database based on data properties

--- src/app/main/results/result.model.ts
+++ src/app/main/results/result.model.ts
@@ -5,12 +5,24 @@ export class Result {
     public link: string; // A result should have a link property of string type.
     public technology: string; // A result should have a technology property of string type.
     public title: string; // A result should have a title property of string type.
+    public searchString ?: string; // A result can have a searchString property of string type.
+    public image ?: string; // A result can have an image property of string type.
 
-    constructor(category: string, keywords: string[], link: string, technology: string, title: string) {
+    constructor(
+        category: string,
+        keywords: string[],
+        link: string,
+        technology: string,
+        title: string,
+        searchString: string,
+        image: string
+    ) {
         this.category = category;
         this.keywords = keywords;
         this.link = link;
         this.technology = technology;
         this.title = title;
+        this.searchString = searchString;
+        this.image = image;
     }
 }
--- src/app/main/results/results.component.ts
+++ src/app/main/results/results.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {Result} from './result.model';
 import {AngularFireDatabase} from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-results',
@@ -10,17 +11,53 @@ import {AngularFireDatabase} from '@angular/fire/database';
 })
 export class ResultsComponent implements OnInit {
   resultObserve: Observable<any>;
+  modifiedResultObserve: Observable<any>;
   results: Result[] = [];
 
   constructor(private db: AngularFireDatabase) {}
 
   ngOnInit() {
     this.resultObserve = this.db.list('documentation').valueChanges();
-    this.resultObserve.subscribe(
-        (results) => {
-          this.results = results;
+    this.modifiedResultObserve = this.resultObserve.pipe(map(
+        (results: Result[]) => {
+          results.map(
+              (item: Result) => {
+                item.searchString = this.mergeArrayItemsToString(
+                    [item.title, item.technology, item.category, ...item.keywords]
+                );
+                switch (item.technology) {
+                  case 'JavaScript':
+                    item.image = './assets/img/javascript.png';
+                    break;
+                  case 'Angular':
+                    item.image = './assets/img/angular.png';
+                    break;
+                  case 'React Redux':
+                    item.image = './assets/img/react.png';
+                    break;
+                }
+              }
+          );
+          return results;
+        }
+    ));
+    this.modifiedResultObserve.subscribe(
+        (result) => {
+          this.results = result;
         }
     );
+  }
+
+  lowerStringRemoveSpace (value: string) {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+
+  mergeArrayItemsToString (arr: string[]) {
+    let mergedString = '';
+    arr.forEach(item => {
+      mergedString = mergedString + this.lowerStringRemoveSpace(item);
+    });
+    return mergedString;
   }
 
 }
